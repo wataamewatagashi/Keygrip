d586b59d970dab155b6c5c5f5ef3d4389d6939db
watagame <watagame2579@gmail.com>
2020-05-04 20:14:01 +0900

    Changed to avoid duplication of scene's names and identifiers

--- src/main/java/me/ichun/mods/keygrip/client/gui/window/WindowSceneSelection.java
+++ src/main/java/me/ichun/mods/keygrip/client/gui/window/WindowSceneSelection.java
@@ -1,16 +1,16 @@
 package me.ichun.mods.keygrip.client.gui.window;
 
+import java.util.ArrayList;
+
 import me.ichun.mods.ichunutil.client.gui.window.element.Element;
 import me.ichun.mods.ichunutil.client.render.RendererHelper;
 import me.ichun.mods.keygrip.client.gui.GuiWorkspace;
 import me.ichun.mods.keygrip.client.gui.window.element.ElementSceneTab;
 import me.ichun.mods.keygrip.common.scene.Scene;
 
-import java.util.ArrayList;
-
 public class WindowSceneSelection extends WindowTopDock
 {
-    public ArrayList<Scene> scenes = new ArrayList<Scene>();
+    public ArrayList<Scene> scenes = new ArrayList<>();
     public int selectedScene;
 
     public WindowSceneSelection(GuiWorkspace parent, int w, int h)
@@ -31,10 +31,7 @@ public class WindowSceneSelection extends WindowTopDock
     @Override
     public void draw(int mouseX, int mouseY) //4 pixel border?
     {
-        if(scenes.isEmpty() || width <= 0)
-        {
-            return;
-        }
+        if(scenes.isEmpty() || width <= 0) return;
         super.draw(mouseX, mouseY);
         RendererHelper.drawColourOnScreen(workspace.currentTheme.tabSideInactive[0], workspace.currentTheme.tabSideInactive[1], workspace.currentTheme.tabSideInactive[2], 255, posX, posY, width, 1, 0);
     }
@@ -42,10 +39,7 @@ public class WindowSceneSelection extends WindowTopDock
     @Override
     public void resized()
     {
-        for(Element element : elements)
-        {
-            element.resized();
-        }
+        elements.forEach(Element::resized);
         if(!workspace.levels.get(0).isEmpty())
         {
             posX = workspace.levels.get(0).get(0).width - 2;
@@ -69,14 +63,29 @@ public class WindowSceneSelection extends WindowTopDock
     @Override
     public void shutdown()
     {
-        for(Scene scene : scenes)
-        {
-            scene.destroy();
-        }
+        scenes.stream().forEach(Scene::destroy);
     }
 
     public void addScene(Scene scene)
     {
+        while (scenes.parallelStream().anyMatch(scene1 -> scene1.identifier.equals(scene.identifier))) {
+            String count = scene.identifier.substring(scene.identifier.length()-2);
+            if  (count.startsWith("_") && count.substring(1).matches("[0-9]")) {
+                int c = Integer.parseInt(count.substring(1)) + 1;
+                scene.identifier = scene.identifier.substring(0, scene.identifier.length()-2) + "_" + c;
+            } else {
+                scene.identifier += "_1";
+            }
+        }
+        while (scenes.parallelStream().anyMatch(scene1 -> scene1.name.equals(scene.name))) {
+            String count = scene.name.substring(scene.name.length()-2);
+            if  (count.startsWith("_") && count.substring(1).matches("[0-9]")) {
+                int c = Integer.parseInt(count.substring(1)) + 1;
+                scene.name = scene.name.substring(0, scene.name.length()-2) + "_" + c;
+            } else {
+                scene.name += "_1";
+            }
+        }
         scenes.add(scene);
         elements.add(new ElementSceneTab(this, 0, 0, 10, 10, elements.size(), scene));
         if(scenes.size() == 1)
@@ -87,42 +96,39 @@ public class WindowSceneSelection extends WindowTopDock
         resized();
     }
 
-    public void removeScene(String ident)
+
+    public void removeScene(String identifier)
     {
         for(int i = scenes.size() - 1; i >= 0; i--)
         {
             Scene project = scenes.get(i);
-            if(project.identifier.equals(ident))
+            if(!(project.identifier.equals(identifier))) continue;
+
+            project.destroy();
+            scenes.remove(i);
+            if(i == selectedScene || selectedScene == scenes.size())
             {
-                project.destroy();
-                scenes.remove(i);
-                if(i == selectedScene || selectedScene == scenes.size())
+                selectedScene--;
+                if(selectedScene < 0 && !scenes.isEmpty())
                 {
-                    selectedScene--;
-                    if(selectedScene < 0 && !scenes.isEmpty())
-                    {
-                        selectedScene = 0;
-                    }
+                    selectedScene = 0;
                 }
-                changeScene(selectedScene);
-                break;
             }
+            changeScene(selectedScene);
+            break;
         }
 
-        ArrayList<Element> els = new ArrayList<Element>(elements);
+        ArrayList<Element> els = new ArrayList<>(elements);
         for(int i = scenes.size() - 1; i >= 0; i--)
         {
             Scene project = scenes.get(i);
             for(Element e : elements)
             {
-                if(e instanceof ElementSceneTab)
-                {
-                    ElementSceneTab tab = (ElementSceneTab)e;
-                    if(tab.info.identifier.equals(project.identifier))
-                    {
-                        tab.id = i;
-                        els.remove(e);
-                    }
+                if(!(e instanceof ElementSceneTab)) continue;
+                ElementSceneTab tab = (ElementSceneTab)e;
+                if(tab.info.identifier.equals(project.identifier)) {
+                    tab.id = i;
+                    els.remove(e);
                 }
             }
         }
@@ -151,14 +157,8 @@ public class WindowSceneSelection extends WindowTopDock
 
     public void changeScene(Scene info)
     {
-        for(int i = 0; i < scenes.size(); i++)
-        {
-            if(scenes.get(i) == info)
-            {
-                changeScene(i);
-                return;
-            }
-        }
+        scenes.stream().filter(scene -> scene == info)
+                .findFirst().ifPresent(this::changeScene);
     }
 
     public void changeScene(int i)
